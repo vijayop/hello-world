a3913a4d062f6fc2ed2bf96fe3a0b9881bfae94f
vijayop <64685522+vijayop@users.noreply.github.com>
2020-05-02 19:57:07 +0530

    Create datas.py

---
+++ datas.py
@@ -0,0 +1,17 @@
+# assigning elements to different  list 
+l1 = []   # empty list 
+l2 = ['1','2','rocket','pocket']
+l3 = ['3','4','try','cry']
+
+# aceesing elements from a tuple
+t1 = ('5','6','harry','cherry')
+print(t1[3])    # 'this will print the 4th element cherry'
+
+# deleting different dictionary elements 
+d1 = {
+      'jan' : 'january',
+      'feb' : 'february',
+      'mar' : 'march',
+      }
+del d1['mar']
+print(d1)    # result
